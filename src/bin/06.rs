fbedad61e9d097fe09cfc1aa82cadc184f75e25f
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2023-12-09 16:37:00 +0100

    -am Day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,11 +1,22 @@
 advent_of_code::solution!(6);
 
-#[fehler::throws(as Option)] pub fn part_one(input: &str) -> u32 {
-	unimplemented!("{input}");
+#[fehler::throws(as Option)]  pub fn part_one(input: &str) -> u32 {
+	let (time, distance) = input.split_once('\n').unwrap();
+	let [time, distance] = [time, distance].map(|s| s.split_once(':').unwrap().1.split_whitespace().map(|e| e.parse::<u32>().unwrap()));
+	let races = time.zip(distance);
+	races.map(|(time, distance)| {
+		(1..time).filter(|hold| {
+			hold*(time-hold) > distance
+		}).count() as u32
+	}).product()
 }
 
-#[fehler::throws(as Option)]  pub fn part_two(_input: &str) -> u32 {
-	fehler::throw!();
+#[fehler::throws(as Option)]  pub fn part_two(input: &str) -> u32 {
+	let (time, distance) = input.split_once('\n').unwrap();
+	let [time, distance] = [time, distance].map(|s| s.split_once(':').unwrap().1.chars().filter(|c| !c.is_whitespace()).collect::<String>().parse::<u64>().unwrap());
+	(1..time).filter(|hold| {
+		hold*(time-hold) > distance
+	}).count() as u32
 }
 
 #[cfg(test)]
@@ -15,12 +26,12 @@ mod tests {
 	#[test]
 	fn test_part_one() {
 		let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-		assert_eq!(result, Some());
+		assert_eq!(result, Some(288));
 	}
 
 	#[test]
 	fn test_part_two() {
 		let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-		assert_eq!(result, None);
+		assert_eq!(result, Some(71503));
 	}
 }
