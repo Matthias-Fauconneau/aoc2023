a941fcb18d49f71d9228e0f213da523427e8718d
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2023-12-10 15:20:05 +0100

    Day 7 - Jokers

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -30,8 +30,39 @@ advent_of_code::solution!(7);
 	}).sum()
 }
 
-#[fehler::throws(as Option)]  pub fn part_two(_input: &str) -> u32 {
-	fehler::throw!();
+#[fehler::throws(as Option)]  pub fn part_two(input: &str) -> u32 {
+	const cards : [char; 13] = ['J','2','3','4','5','6','7','8','9','T','Q','K','A'];
+	let mut hands = input.lines().map(|line| {
+		let (hand, bid) = line.split_once(' ').unwrap();
+		fn from_iter<T, const N: usize>(mut iter: impl Iterator<Item=T>) -> [T; N] { [(); N].map(|_| iter.next().unwrap()) }
+		let hand : [_; 5] = from_iter(hand.chars().map(|c| cards.iter().position(|&v| c==v).unwrap()));
+		let mut histogram = [0; 13];
+		for card in hand { histogram[card] += 1; }
+		let mut jokers = histogram[0];
+		histogram.sort();
+		histogram.reverse();
+		for count in &mut histogram {
+			let act = u32::min(5-*count, jokers);
+			*count += act;
+			jokers -= act;
+		}
+		let type_rank = match histogram {
+			[5, ..] => 7,
+			[4, ..] => 6,
+			[3, 2, ..] => 5,
+			[3, ..] => 4,
+			[2, 2, ..] => 3,
+			[2, ..] => 2,
+			_ => 1,
+		};
+		(type_rank, hand, bid.parse::<u32>().unwrap())
+	}).collect::<Box<_>>();
+	hands.sort();
+	hands.iter().enumerate().map(|(index, (_, _, bid))| {
+		let rank = index+1;
+		println!("{rank} {bid}");
+		rank as u32*bid
+	}).sum()
 }
 
 #[cfg(test)]
@@ -47,6 +78,6 @@ mod tests {
 	#[test]
 	fn test_part_two() {
 		let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-		assert_eq!(result, None);
+		assert_eq!(result, Some(5905));
 	}
 }
