814c81f9fc2b93987e45372475d0a2fcb0e6390a
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2023-12-10 19:03:46 +0100

    reverse

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -13,8 +13,26 @@ advent_of_code::solution!(9);
 	}).sum()
 }
 
-#[fehler::throws(as Option)]  pub fn part_two(_input: &str) -> u32 {
-	fehler::throw!();
+#[fehler::throws(as Option)]  pub fn part_two(input: &str) -> i32 {
+	/*input.lines().map(|line| {
+		let mut stack = Vec::from([line.split_whitespace().map(|s| s.parse::<i32>().expect(s)).collect::<Box<_>>()]);
+		loop {
+			let next = stack.last().unwrap().array_windows().map(|[prev, next]| next-prev).collect::<Box<_>>();
+			if next.iter().all(|&r| r==0) { break; }
+			stack.push(next);
+		}
+		dbg!(-stack.iter().fold(0, |first, history| history.first().unwrap()-first))
+	}).sum()*/
+	input.lines().map(|line| {
+		let mut stack = Vec::from([line.split_whitespace().map(|s| s.parse::<i32>().expect(s)).collect::<Box<_>>()]);
+		stack[0].reverse();
+		loop {
+			let next = stack.last().unwrap().array_windows().map(|[prev, next]| next-prev).collect::<Box<_>>();
+			if next.iter().all(|&r| r==0) { break; }
+			stack.push(next);
+		}
+		stack.iter().map(|history| history.last().unwrap()).sum::<i32>()
+	}).sum()
 }
 
 #[cfg(test)]
@@ -30,6 +48,6 @@ mod tests {
 	#[test]
 	fn test_part_two() {
 		let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-		assert_eq!(result, None);
+		assert_eq!(result, Some(2));
 	}
 }
