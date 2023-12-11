91b1ad36d0912ab3bbc785924318110e2767b7ab
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2023-12-11 22:45:55 +0100

    virtual expansion

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -11,8 +11,20 @@ advent_of_code::solution!(11);
 	println!("{:?}", galaxies.iter().format(" "));
 	galaxies.iter().tuple_combinations().map(|(&(ax,ay),&(bx,by))| ax.abs_diff(bx)+ay.abs_diff(by)).sum::<usize>()
 }
-#[fehler::throws(as Option)]  pub fn part_two(_input: &str) -> u32 {
-	fehler::throw!();
+
+#[fehler::throws(as Option)]  pub fn part_two(input: &str) -> usize {
+	let space = input.lines().map(|row| row.chars().collect::<Box<_>>()).collect::<Box<_>>();
+	use itertools::Itertools;
+	println!("\n{}", space.iter().format_with("\n", |row, f| f(&row.iter().collect::<String>())));
+	fn space_without_galaxies(space: &[Box<[char]>]) -> Box<[usize]> { space.iter().enumerate().filter_map(|(i, row)| row.iter().all(|&c| c=='.').then_some(i)).collect() }
+	let ref rows_without_galaxies = space_without_galaxies(&*space);
+	fn transpose(space: &[Box<[char]>]) -> Box<[Box<[char]>]> { (0..space[0].len()).map(move |j| (0..space.len()).map(move |i| space[i][j]).collect::<Box<_>>()).collect::<Box<_>>() }
+	let ref columns_without_galaxies = space_without_galaxies(&*transpose(&*space));
+	let galaxies = space.iter().enumerate().map(|(i, row)| row.iter().enumerate().filter_map(move |(j, &c)| (c=='#').then_some([i,j]))).flatten().collect::<Box<_>>();
+	println!("{:?}", galaxies.iter().format(" "));
+	use std::cmp::{min, max};
+	galaxies.iter().tuple_combinations().map(|(a,b)| a.iter().zip(b).zip([rows_without_galaxies, columns_without_galaxies]).map(|((&a,&b), expand)| 
+		a.abs_diff(b)+(min(a,b)..max(a,b)).filter(|i| expand.contains(i)).count()*(1000000-1)).sum::<usize>()).sum::<usize>()
 }
 
 #[cfg(test)]
@@ -28,6 +40,6 @@ mod tests {
 	#[test]
 	fn test_part_two() {
 		let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-		assert_eq!(result, Some(8410));
+		assert_eq!(result, Some(82000210));
 	}
 }
